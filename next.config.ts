15f06d7825dd3648218693d03d1504d7142c8843
rjuna72 <rjun5689@gmail.com>
2026-09-16 15:09:19 +1000

    s'

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
-const config: NextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
-  poweredByHeader: false,
-  images: { unoptimized: true },
+  basePath: "/AaramFusion",
+  images: {
+    unoptimized: true,
+  },
 };
-export default config;
+
+export default nextConfig;
